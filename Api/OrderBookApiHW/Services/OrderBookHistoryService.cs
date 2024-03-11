78b64b2d3cf061fb183695d56b004a784363097f
Vasyl Klyzhenko <vklyzhenko@b2bsoft.com>
2024-03-11 22:08:35 +0100

    fix rendering, fix ordering

--- Api/OrderBookApiHW/Services/OrderBookHistoryService.cs
+++ Api/OrderBookApiHW/Services/OrderBookHistoryService.cs
@@ -24,37 +24,35 @@ namespace OrderBookApiHW.Services
         {
             var result = new BookHistoryModel();
 
-            var startOfDay = upToDateTime.Date;
-            var endOfDay = upToDateTime.Date.AddDays(1).AddTicks(-1);
+            var startOfDay = upToDateTime.Date.AddDays(-1);
 
             // Get history of day for bids and asks
             var bidsQuery = _loggerContext.OrderBookLogs
-                .Where(book => book.Amount > 0 && book.OrderTime >= startOfDay && book.OrderTime <= endOfDay)
+                .Where(book => book.Amount > 0 && book.OrderTime >= startOfDay && book.OrderTime <= upToDateTime)
                 .AsEnumerable();
 
             var asksQuery = _loggerContext.OrderBookLogs
-                .Where(book => book.Amount < 0 && book.OrderTime >= startOfDay && book.OrderTime <= endOfDay)
+                .Where(book => book.Amount < 0 && book.OrderTime >= startOfDay && book.OrderTime <= upToDateTime)
                 .AsEnumerable();
 
             // Group orders, remove 
-            result.Bids = await ProcessBooks(bidsQuery);
-            result.Asks = await ProcessBooks(asksQuery);
+            result.Bids = await ProcessBooks(bidsQuery, true);
+            result.Asks = await ProcessBooks(asksQuery, false);
 
             return result;
         }
 
-        private async Task<Dictionary<string, Book>> ProcessBooks(IEnumerable<OrderBookLogsEntity> query)
+        
+        private async Task<Dictionary<string, Book>> ProcessBooks(IEnumerable<OrderBookLogsEntity> query, bool isBid)
         {
             var filteredGroups = query
                 .GroupBy(book => book.Price)
-                .Where(group => group.All(book => book.Count > 0)) // if price contains any Count == 0 remove that book
-                .Select(group => group
-                    .OrderByDescending(book => book.OrderTime)
-                    .First()) // Get most recent book for the price point
-                .OrderBy(book => book.Price)
-                .Take(Depth);
+                .Select(group => group.OrderByDescending(book => book.OrderTime).First())
+                .Where(book => book.Count != 0);
 
-            return filteredGroups.ToDictionary(
+            var orderedGroups = isBid ? filteredGroups.OrderByDescending(book => book.Price) : filteredGroups.OrderBy(book => book.Price);
+
+            return orderedGroups.Take(Depth).ToDictionary(
                 book => book.Price.ToString(),
                 book => new Book
                 {
